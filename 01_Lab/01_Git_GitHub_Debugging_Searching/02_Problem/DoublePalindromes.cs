8af5d2af1a3ff985418e67261ef77a6f6709a352
HristoMachikov <hristo2635@abv.bg>
2017-03-06 01:32:26 +0200

    Solve Propblem 2 from 01_Lab

--- 01_Lab/01_Git_GitHub_Debugging_Searching/02_Problem/DoublePalindromes.cs
+++ 01_Lab/01_Git_GitHub_Debugging_Searching/02_Problem/DoublePalindromes.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Numerics;
     using System.Text;
@@ -10,60 +11,77 @@
     {
         public static void Main()
         {
-            List<int> result = new List<int>() { 0, 1 };
-            int index = 2;
-            while (result.Count < 29)
+            //List<int> result = new List<int>() { 0, 1 };
+            //int index = 2;
+            //while (result.Count < 29)
+            //{
+            //    int number = index;
+            //    int secondCheck = 0;
+            //    string numberToString = index.ToString();
+            //    if (numberToString.Length > 1)
+            //    {
+            //        for (int i = 0; i < numberToString.Length / 2; i++)
+            //        {
+            //            if (numberToString[i] == numberToString[numberToString.Length - 1 - i])
+            //            {
+            //                secondCheck++;
+            //            }
+            //            else
+            //            {
+            //                break;
+            //            }
+            //        }
+            //    }
+
+            //    if (secondCheck == numberToString.Length / 2)
+            //    {
+            //        string outputArray = Convert.ToString(number, 2);
+
+            //        //string output = string.Empty;
+            //        //while (number != 0)
+            //        //{
+            //        //    BigInteger toBeConcat = number % 2;
+            //        //    output += toBeConcat;
+            //        //    number = number / 2;
+            //        //}
+
+            //        //char[] outputArray = output.ToCharArray();
+
+            //        int firstCheck = 0;
+            //        for (int i = 0; i < outputArray.Length / 2; i++)
+            //        {
+            //            if (outputArray[i] == outputArray[outputArray.Length - 1 - i])
+            //            {
+            //                firstCheck++;
+            //            }
+            //            else
+            //            {
+            //                break;
+            //            }
+            //        }
+
+            //        if (firstCheck == outputArray.Length / 2)
+            //        {
+            //            result.Add(index);
+            //        }
+            //    }
+
+            //    index++;
+            //}
+
+            //Console.WriteLine(string.Join(", ", result));
+
+            int numb=int.Parse(Console.ReadLine());
+            string[] inputArray = File.ReadAllLines(@"../../Input.txt");
+            List<string> output = new List<string>();
+            foreach (var line in inputArray)
             {
-                int number = index;
-                string output = string.Empty;
-                while (number != 0)
-                {
-                    BigInteger toBeConcat = number % 2;
-                    output += toBeConcat;
-                    number = number / 2;
-                }
-
-                char[] outputArray = output.ToCharArray();
-
-                int firstCheck = 0;
-                for (int i = 0; i < outputArray.Length / 2; i++)
-                {
-                    if (outputArray[i] == outputArray[outputArray.Length - 1 - i])
-                    {
-                        firstCheck++;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                int secondCheck = 0;
-                string numberToString = index.ToString();
-                if (numberToString.Length > 1)
-                {
-                    for (int i = 0; i < numberToString.Length / 2; i++)
-                    {
-                        if (numberToString[i] == numberToString[numberToString.Length - 1 - i])
-                        {
-                            secondCheck++;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                if (firstCheck == outputArray.Length / 2 && secondCheck == numberToString.Length / 2)
-                {
-                    result.Add(index);
-                }
-
-                index++;
+                string[] result = line
+                    .Trim()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                output.Add(result[3]);
             }
-
-            Console.WriteLine(string.Join(", ", result));
+            Console.WriteLine(string.Join(", ", output));
         }
     }
 }
